6fb5f64d4b737066bd7fd894b947e60c1589db69
Deygozulviter <deygozulviter@gmail.com>
2021-10-13 01:40:19 +0700

    Final Commit

---
+++ Assets/Scripts/BackMenu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class BackMenu : MonoBehaviour
+{ 
+    [SerializeField] private Button backButton;
+
+    private void Start()
+    {
+        backButton.onClick.AddListener(() =>
+        {
+            SceneManager.LoadScene(9);
+        });
+    }
+}
---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    //RigidBody 2D Bola
+    private Rigidbody2D rigidBody2D;
+    //Menggerakan Bola
+    public KeyCode upButton = KeyCode.W;
+    public KeyCode downButton = KeyCode.S;
+    public KeyCode leftButton = KeyCode.A;
+    public KeyCode rightButton = KeyCode.D;
+    public float speed = 10.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody2D = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        // Dapatkan kecepatan bola sekarang.
+        Vector2 velocity = rigidBody2D.velocity;
+
+        // Controller
+        if (Input.GetKey(upButton))
+        {
+            velocity.y = speed;
+        }
+        else if (Input.GetKey(downButton))
+        {
+            velocity.y = -speed;
+        }
+        else if (Input.GetKey(leftButton))
+        {
+            velocity.x = -speed;
+        }
+        else if (Input.GetKey(rightButton))
+        {
+            velocity.x = speed;
+        }
+        else
+        {
+            velocity.y = 0.0f;
+            velocity.x = 0.0f;
+        }
+
+        // Masukkan kembali kecepatannya ke rigidBody2D.
+        rigidBody2D.velocity = velocity;
+    }
+}
